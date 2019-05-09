29e29539f0e25d557da1f56ff1b5645959506df1
Matheus de Holanda Pereira Ribeiro <35711103822@info.local>
2019-05-09 16:41:40 -0300

    cadastro quase finalizado

--- FrontEnd/RNRoman/src/pages/cadastrar.js
+++ FrontEnd/RNRoman/src/pages/cadastrar.js
@@ -1,10 +1,34 @@
 import React, { Component } from 'react';
-import { Text, Image, StyleSheet, View, FlatList } from "react-native";
+import { Text, StyleSheet, View, TouchableOpacity} from "react-native";
 import Cabecalho from '../componentes/cabecalho'
 import { TextInput } from 'react-native-gesture-handler';
-import Textarea from 'react-native-textarea';
+import api from '../services/api'
+import Axios from 'axios';
 
-class cadastrar extends Component {
+export default class cadastrar extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            nome:""
+            ,tema:""
+            ,descricao:""
+        }
+    }
+    cadastrarProjeto = async() =>{
+        let dados = {
+            nome: this.state.nome,
+            tema: this.state.tema,
+            descricao: this.state.descricao
+        };
+
+        await Axios.post(api, dados, {
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+        .catch(error => console.warn(error))
+        this.props.navigation.navigate('/listagem');
+    }
 
     render() {
         return (
@@ -14,13 +38,27 @@ class cadastrar extends Component {
                 <TextInput
                     style={styles.input}
                     textContentType="text"
+                    onChangeText={nome=>this.setState({nome})}
                     placeholder="Nome"
                 />
                 <TextInput
                     style={styles.input}
                     textContentType="text"
+                    onChangeText={tema=>this.setState({tema})}
                     placeholder="Tema"
                 />
+                <TextInput
+                    style={styles.input}
+                    textContentType="text"
+                    onChangeText={descricao=>this.setState({descricao})}
+                    placeholder="Descrição"
+                />
+                <TouchableOpacity
+                    style={styles.button}
+                    onPress={this.cadastrarProjeto}
+                >
+                    <Text style={styles.buttontxt}>Cadastrar</Text>
+                </TouchableOpacity>
             </View>
         );
     }
@@ -38,7 +76,27 @@ const styles = StyleSheet.create({
         backgroundColor: "transparent",
         borderBottomColor: "green",
         borderBottomWidth: 2
+    },
+    button:{
+        height:39,
+        width:124,
+        backgroundColor:"#388E3C",
+        borderBottomColor:"#1B5E20",
+        borderWidth:0.5,
+        borderRadius:5,
+        position:"relative",
+        left:140,
+        textAlign:"center",
+        top:150,
+        shadowOffset:{  width: 100,  height: 100,  },
+        shadowColor: 'black',
+        shadowOpacity: 1.0,
+    },
+    buttontxt:{
+        textAlign:"center",
+        color:"white",
+        fontSize:20,
+        position:"relative",
+        top:5,
     }
 });
-
-export default cadastrar;
